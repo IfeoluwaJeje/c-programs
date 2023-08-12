c15a922861b4af5dd40b3df09976ef59bb31fe18
Theophilus Samuel <psalmomo@gmail.com>
2023-08-12 14:02:53 +0100

    updated add

--- Operators/add.c
+++ Operators/add.c
@@ -7,3 +7,32 @@ void main()
     c = a + b;
     printf("The Sum is %d", c);
 }
+
+// Add more numbers togetheer
+#include <stdio.h>
+
+int main()
+{
+    int numCount, result;
+
+    // Get the number of values to add
+    printf("Enter the number of values to add: ");
+    scanf("%d", &numCount);
+
+    // Get the input numbers
+    printf("Enter %d numbers:\n", numCount);
+
+    // Perform the addition
+    result = 0;
+    for (int i = 0; i < numCount; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        result += num;
+    }
+
+    // Print the result
+    printf("The Sum is %d\n", result);
+
+    return 0;
+}
